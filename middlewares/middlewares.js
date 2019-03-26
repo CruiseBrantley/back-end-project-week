87fb1add3525c19ab8920be8706d73a71b9d19f5
Cruise Brantley <cruise.brantley@gmail.com>
2019-03-25 22:46:04 -0500

    updated auth middleware

--- middlewares/middlewares.js
+++ middlewares/middlewares.js
@@ -3,23 +3,36 @@ const jwt = require("jsonwebtoken");
 const User = require("../users/userModel");
 const config = require("../config");
 
-const authenticate = (req, res, next) => {
-  let token = req.get("Authorization");
-  if (token != undefined) token = token.replace("Bearer ", "");
-  if (token) {
-    jwt.verify(token, config.secret, (err, decoded) => {
-      if (err) return res.status(422).json(err);
-      req.decoded = decoded;
-      next();
-    });
-  } else {
-    console.log("Repelled Invader", token);
-    return res.status(403).json({
-      error: "You're not allowed in here!"
-    });
-  }
+const authenticate = async (req, res, next) => {
+	const token = req.get("Authorization");
+	const username = req.get("username");
+	const password = req.get("password");
+	if (token) {
+		token = token.replace("Bearer ", "");
+		jwt.verify(token, config.secret, (err, decoded) => {
+			if (err) return res.status(422).json(err);
+			req.decoded = decoded;
+			next();
+		});
+	} else if (username && password) {
+		const user = await User.findOne({ username });
+		if (user && user.validatePassword(password)) {
+			req.decoded = {
+				username: user.username,
+				id: user._id
+			};
+			next();
+		} else {
+			res.status(422).json({ error: "Invalid Credentials." });
+		}
+	} else {
+		console.log("Repelled Invader", token);
+		return res.status(403).json({
+			error: "You're not allowed in here!"
+		});
+	}
 };
 
 module.exports = {
-  authenticate
+	authenticate
 };
