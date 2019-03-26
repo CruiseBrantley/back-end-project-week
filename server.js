3b9532f6cea00649de911062f0da5c908de9dd57
Cruise Brantley <cruise.brantley@gmail.com>
2019-03-26 15:13:23 -0500

    Testing CORS change

--- server.js
+++ server.js
@@ -13,30 +13,30 @@ const noteController = require("./notes/noteController");
 const userController = require("./users/userController");
 
 mongoose
-  .connect(
-    `mongodb://${config.mongo.username}:${
-      config.mongo.password
-    }@ds139970.mlab.com:39970/cruise-notes`
-  )
-  .then(() => {
-    console.log("connected to database");
-  })
-  .catch(err => {
-    console.log("error connecting to database");
-  });
+	.connect(
+		`mongodb://${config.mongo.username}:${
+			config.mongo.password
+		}@ds139970.mlab.com:39970/cruise-notes`
+	)
+	.then(() => {
+		console.log("connected to database");
+	})
+	.catch(err => {
+		console.log("error connecting to database");
+	});
 
-server.use(cors({}));
+server.use(cors());
 server.use(express.json());
 server.use(morgan("combined"));
 
 server.get("/", (req, res) => {
-  res.json({ Message: "Server's not broken" });
+	res.json({ Message: "Server's not broken" });
 });
 
 server.use("/api/notes", authenticate, noteController);
 server.use("/api/users", userController);
 
 server.listen(port, err => {
-  if (err) console.log(err);
-  console.log(`Focus your attack on port ${port}`);
+	if (err) console.log(err);
+	console.log(`Focus your attack on port ${port}`);
 });
