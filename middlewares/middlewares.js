cf4f8722e59e7fdad8078c9e2832e658aa28392f
Cruise Brantley <cruise.brantley@gmail.com>
2019-03-26 19:30:49 -0500

    Added middleware catch

--- middlewares/middlewares.js
+++ middlewares/middlewares.js
@@ -7,29 +7,33 @@ const authenticate = async (req, res, next) => {
 	const token = req.get("Authorization");
 	const username = req.get("username");
 	const password = req.get("password");
-	if (token) {
-		token = token.replace("Bearer ", "");
-		jwt.verify(token, config.secret, (err, decoded) => {
-			if (err) return res.status(422).json(err);
-			req.decoded = decoded;
-			next();
-		});
-	} else if (username && password) {
-		const user = await User.findOne({ username });
-		if (user && user.validatePassword(password)) {
-			req.decoded = {
-				username: user.username,
-				id: user._id
-			};
-			next();
+	try {
+		if (token) {
+			token = token.replace("Bearer ", "");
+			jwt.verify(token, config.secret, (err, decoded) => {
+				if (err) return res.status(422).json(err);
+				req.decoded = decoded;
+				next();
+			});
+		} else if (username && password) {
+			const user = await User.findOne({ username });
+			if (user && user.validatePassword(password)) {
+				req.decoded = {
+					username: user.username,
+					id: user._id
+				};
+				next();
+			} else {
+				res.status(422).json({ error: "Invalid Credentials." });
+			}
 		} else {
-			res.status(422).json({ error: "Invalid Credentials." });
+			console.log("Repelled Invader", token);
+			return res.status(403).json({
+				error: "You're not allowed in here!"
+			});
 		}
-	} else {
-		console.log("Repelled Invader", token);
-		return res.status(403).json({
-			error: "You're not allowed in here!"
-		});
+	} catch {
+		res.status(500).json("Encountered an error.");
 	}
 };
 
